df854bbecfc6dd9c7b689a62d66dd44c2c1b5544
Mustafa Zaki Assagaf <mustafa.segf@gmail.com>
2023-01-28 18:12:19 +0700

    trying to make workspace

--- flake.nix
+++ flake.nix
@@ -18,14 +18,32 @@
       with pkgs;
       {
         devShells.default = mkShell {
+          nativeBuildInputs = [ pkgs.bashInteractive ];
+
+          NIX_LD_LIBRARY_PATH = lib.makeLibraryPath [
+            stdenv.cc.cc
+            openssl
+          ];
+
+          NIX_LD = pkgs.runCommand "ld.so" {} ''
+            ln -s "$(cat '${pkgs.stdenv.cc}/nix-support/dynamic-linker')" $out
+          '';
+
           buildInputs = [
+            nodePackages.prisma
             openssl
             pkg-config
-            (rust-bin.selectLatestNightlyWith
-              (toolchain: toolchain.complete.override {
-                targets = [ "wasm32-unknown-unknown" ];
-              }))
+            stdenv.cc.cc.lib
+            (rust-bin.fromRustupToolchainFile ./rust-toolchain.toml)
           ];
+          shellHook = with pkgs; ''
+            export PRISMA_MIGRATION_ENGINE_BINARY="${prisma-engines}/bin/migration-engine"
+            export PRISMA_QUERY_ENGINE_BINARY="${prisma-engines}/bin/query-engine"
+            export PRISMA_QUERY_ENGINE_LIBRARY="${prisma-engines}/lib/libquery_engine.node"
+            export PRISMA_INTROSPECTION_ENGINE_BINARY="${prisma-engines}/bin/introspection-engine"
+            export PRISMA_FMT_BINARY="${prisma-engines}/bin/prisma-fmt"
+          '';
+
         };
       }
     );
